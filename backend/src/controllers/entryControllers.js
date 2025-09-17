b4744ef7d7714ba461a44fb4827599dad0916670
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-09-17 18:22:55 +0530

    fix: ensure proper fetching of entries with excluded fields in getAllEntries function

--- backend/src/controllers/entryControllers.js
+++ backend/src/controllers/entryControllers.js
@@ -3,11 +3,23 @@ import Entry from "../models/Entry.js";
 export async function getAllEntries(req, res){
     try {
         const userId = req.body.userId;
-        const entries = Entry.find({userId});
+        const entries = await Entry.find({userId}, {_id : 0, __v : 0, createdAt: 0});
         res.status(200).json({message : "Entries Fetched", entries : entries});
     } catch (error) {
         console.log(error);
         res.status(500).json({message: "Server Side Error"})
+    }
+}
+
+export async function insertEntry(req, res) {
+    try {                                                                                                                       
+        const {userId, type, category, subCategory, amount, description} = req.body;
+        const entry = new Entry({userId, type, category, subCategory, amount, description});
+        await entry.save();
+        res.status(200).json({message : "Successfully inserted", entry : entry});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Server Side Error"}) 
     }
 }
 
