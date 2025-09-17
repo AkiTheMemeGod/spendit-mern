622686ef660e21911b3597f260e3a116843e3aa7
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-09-17 17:51:24 +0530

    fix: correct import statements and ensure entry routes are properly registered

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './utils/db.js';
-import aiRoutes from './Routes/spenditbotRoutes.js'
-
+import aiRoutes from './Routes/spenditbotRoutes.js';
+import entryRoutes from './Routes/entryRoutes.js';
 
 dotenv.config("./");
 const app = express();
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/spenditbot", aiRoutes);
-
+app.use("/api/entry", entryRoutes)
 
 connectDB().then(()=>{
 app.listen(5001, () => {
