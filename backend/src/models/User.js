f9c137cd7e9c9df106e6d445d662e0b6e6515a22
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-09-17 17:46:01 +0530

    feat: add User model with schema for user management

---
+++ backend/src/models/User.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+    username : {type: String, required: true},
+    email : {type: String, required: true},
+    passwordHash : {type: String , required: true},
+    profilePic : {type: String, required: true}
+});
+
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
