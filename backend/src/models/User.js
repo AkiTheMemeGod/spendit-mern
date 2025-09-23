0e7287cc316e3eeb99bb55d9da3aceb976845e1d
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-09-23 23:53:20 +0530

    fix: update profilePic field to be optional in user schema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     username : {type: String, required: true},
     email : {type: String, required: true},
     passwordHash : {type: String , required: true},
-    profilePic : {type: String, required: true}
+    profilePic : {type: String, required: false}
 });
 
 
