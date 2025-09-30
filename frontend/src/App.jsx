9e7e1cd8f1b2b2f210be25b53518e208ffda3f02
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-09-30 23:47:11 +0530

    feat: update App component layout to include Footer and enhance responsiveness

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import AuthPage from "./pages/AuthPage.jsx";
+import Footer from "./components/Footer.jsx";
 
 
 const PrivateRoute = ({ children }) => {
@@ -10,19 +11,21 @@ const PrivateRoute = ({ children }) => {
 
 const App = () => {
   return (
-    <div className="relative h-full w-full">
-      <Routes>
-        <Route path="/" element={<AuthPage />} />
-
-        <Route
-          path="/home"
-          element={
-            <PrivateRoute>
-              <HomePage />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
+    <div className="relative min-h-screen flex flex-col">
+      <div className="flex-1">
+        <Routes>
+          <Route path="/" element={<AuthPage />} />
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute>
+                <HomePage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 };
