555d743c287a02f10941e4fe770257b0675eace8
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-09-30 23:47:51 +0530

    feat: refactor HomePage to manage account data and improve entry fetching

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,34 +1,40 @@
-import {useState, useEffect} from 'react'
-import AnimatedList from '../components/AnimatedList.jsx'
-import axiosUtil from '../services/axios.js'
-import toast from 'react-hot-toast';
-import getUserIdFromToken from '../services/jwt_utils.js';
+import { useState, useEffect } from "react";
+import BalanceCard from "../components/BalanceCard.jsx";
+import AnimatedList from "../components/AnimatedList.jsx";
+import toast from "react-hot-toast";
+import axiosUtil from "../services/axios.js";
+import getUserIdFromToken from "../services/jwt_utils";
 const HomePage = () => {
-  const [Entries, setEntries] = useState([]);
+  const [accountData, setAccountData] = useState({ entries: [], balance: 0, incomes: 0, expenses: 0 });
   useEffect(() => {
-    const fetchEtnries = async () => {
-      console.log(getUserIdFromToken());
+    const fetchEntries = async () => {
       try {
         const res = await axiosUtil.entry.get(`?userId=${getUserIdFromToken()}`);
-        console.log(res.data);
-        setEntries(res.data.entries);
+        console.log(res);
+        setAccountData(res.data);
       } catch (error) {
-        console.error("Error fetching products:", error);
+        console.log(error);
         toast.error("Failed to load products");
       }
     };
-    fetchEtnries();
+    fetchEntries();
   }, []);
- 
 
   return (
-<AnimatedList
-  items={Entries}
-  // onItemSelect={(item, index) => console.log(item, index)}
-  showGradients={true}
-  enableArrowNavigation={true}
-  displayScrollbar={false}
-/>  )
-}
+    <div>
+      <BalanceCard
+        balance={accountData.balance}
+        incomes={accountData.incomes}
+        expenses={accountData.expenses}
+      />
+      <AnimatedList
+        items={accountData.entries}
+        showGradients={true}
+        enableArrowNavigation={true}
+        displayScrollbar={false}
+      />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
